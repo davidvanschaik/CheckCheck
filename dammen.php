adb2aba9c0cf39913c3ad3a75abea85dcfd8faf7
David van Schaik <davidvan.schaik00@gmail.com>
2024-12-10 09:58:33 +0100

    Small fix

--- dammen.php
+++ dammen.php
@@ -7,6 +7,4 @@ require "vendor/autoload.php";
 use Game\Checkers;
 use Player\Player;
 
-$spel = new Checkers([new Player('white'), new Player('black')]);
-
-$spel->start();
+(new Checkers([new Player('white'), new Player('black')]))->start();
