184e07ea154bd4b1ef18a24e798ecf5ec13cc0b9
David van Schaik <davidvan.schaik00@gmail.com>
2024-12-10 09:53:19 +0100

    Game works. Only need to add King functionality and validate winner

--- Game/Checkers.php
+++ Game/Checkers.php
@@ -4,7 +4,8 @@ namespace Game;
 
 use Board\Board;
 use Board\Position;
-use Validation\ValidateAvailableCaptures;
+use Player\Move;
+use Validation\RuleValidator;
 
 class Checkers
 {
@@ -24,25 +25,31 @@ class Checkers
     public function start(): void
     {
         while (! $this->winner) {
-            $this->board->showBoard($this->board);
-            [$startPosition, $endPosition] = $this->getPlayerMove();
-            $move = $this->players[$this->activePlayer]->setMove($startPosition, $endPosition);
-            var_dump((new ValidateAvailableCaptures())->validate($move, $this->board, $this->players[$this->activePlayer]));
+            $this->board->showBoard();
+            $move = $this->getPlayerMove();
+            $this->validateRules($move);
         }
     }
 
-    private function getPlayerMove(): array
+    private function getPlayerMove(): Move
     {
         $color = ucfirst($this->players[$this->activePlayer]->color);
         $startPosition = $this->setPosition(readline("\n \nIt's $color's turn. Please select a stone to move. (x,y) "));
-        $endPosition = $this->setPosition(readline("Choose the destination for your stone (format: x,y) "));
-
-        return [$startPosition, $endPosition];
+        $endPosition = $this->setPosition(readline("Choose where you want to move your stone. (x,y) "));
+        return $this->players[$this->activePlayer]->setMove($startPosition, $endPosition);
     }
 
     private function setPosition(string $position): Position
     {
         list($x, $y) = explode(',', $position);
         return new Position($x, $y);
+    }
+
+    private function validateRules(Move $move): void
+    {
+        if ((new RuleValidator())->validateMove($move, $this->board, $this->players[$this->activePlayer])) {
+            $this->players[$this->activePlayer]->executeMove($move, $this->board);
+            $this->activePlayer = 1 - $this->activePlayer;
+        }
     }
 }
