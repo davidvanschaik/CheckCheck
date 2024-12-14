2102adacc9ae3498461999c395ffe9cbf1e45473
David van Schaik <davidvan.schaik00@gmail.com>
2024-12-14 07:41:38 +0100

    Added methods for king logic

--- Player/Pieces.php
+++ Player/Pieces.php
@@ -6,13 +6,16 @@ abstract class Pieces
 {
     public string $color;
 
-    public function copyClassName(): string
+    public function captureBackwards(self $stone): bool
     {
-        
+        return $stone instanceof King;
     }
 
-    public function kanAchteruitSlaan(): bool
+    public function moveDirection(self $stone): array
     {
-        
+        if ($stone instanceof King) {
+            return [-1, +1];
+        }
+        return $this->color === 'white' ? [-1] : [+1];
     }
 }
